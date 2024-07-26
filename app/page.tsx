7ac7c4112e8f728dee6debc9c7244ec26db2859d
williamtuominiemi <william.m.tuominiemi@gmail.com>
2024-07-26 23:44:36 +0300

    Some sophistication

--- app/page.tsx
+++ app/page.tsx
@@ -15,34 +15,37 @@ export default function Home() {
 
   const [error, setError] = useState<string | null>(null);
 
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+
   useEffect(() => {
     getLocalStream(setError);
-
-    const getAudios = async () => {
-      try {
-        const response = await fetch('/api/audio');
-        if (!response.ok) {
-          throw new Error('Failed to fetch audios');
-        }
-        const data: AudioData[] = await response.json();
-        console.log(data);
-        setAudios(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
     getAudios();
   }, []);
 
+  const getAudios = async () => {
+    try {
+      const response = await fetch('/api/audio');
+      if (!response.ok) {
+        throw new Error('Failed to fetch audios');
+      }
+      const data: AudioData[] = await response.json();
+      console.log(data);
+      setAudios(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const handleStartRecording = () => {
+    if (audio) setAudio(null);
+    setIsRecording(true);
     startRecording();
   };
 
   const handleStopRecording = async () => {
+    setIsRecording(false);
     const base64Audio = await stopRecording();
     setAudio(base64Audio);
-    console.log('Base64 Audio String:', base64Audio);
   };
 
   const uploadSound = async () => {
@@ -51,6 +54,13 @@ export default function Home() {
         method: 'POST',
         body: JSON.stringify({ audio }),
       });
+
+      if (response.ok) {
+        await getAudios();
+        if (audio) setAudio(null);
+      } else {
+        console.error('Failed to upload audio');
+      }
     } catch (error) {
       console.error('Error fetching:', error);
     }
@@ -58,21 +68,17 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Sound Thing</p>
+      <h1>Speakr</h1>
       {error && <p className="error-message">{error}</p>}
 
-      {audios && (
-        <div>
-          {audios.map((audio) => (
-            <div key={audio.id}>
-              <audio src={audio.base64} controls />
-            </div>
-          ))}
-        </div>
-      )}
       <div>
-        <button onClick={handleStartRecording}>Start Recording</button>
-        <button onClick={handleStopRecording}>Stop Recording</button>
+        <p>Speak: </p>
+        <button className="text-3xl" onClick={handleStartRecording} disabled={isRecording}>
+          ▶️
+        </button>
+        <button className="text-3xl" onClick={handleStopRecording} disabled={!isRecording}>
+          ⏹️
+        </button>
       </div>
       {audio && (
         <div>
@@ -80,6 +86,17 @@ export default function Home() {
           <button type="button" onClick={uploadSound}>
             Upload
           </button>
+        </div>
+      )}
+
+      {audios && (
+        <div>
+          <p>What others have to say:</p>
+          {audios.map((audio) => (
+            <div className="p-1" key={audio.id}>
+              <audio src={audio.base64} controls />
+            </div>
+          ))}
         </div>
       )}
     </main>
