a5ccb0e3b7021001278e91ee0db3cf6f584adde5
williamtuominiemi <william.m.tuominiemi@gmail.com>
2024-07-28 00:07:59 +0300

    custom scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
 
   return (
     <main className="flex flex-col h-screen w-2/3 bg-gray-800 mx-auto relative rounded-lg">
-      <div className="flex flex-col justify-center items-center flex-1 overflow-y-auto">
+      <div className="flex flex-col justify-center items-center flex-1 overflow-y-auto scrollbar-thin">
         <div className="w-1/2">
           <h1 className="text-white mb-4 pt-5">🗣️ Speakr</h1>
           <p className="text-white mb-4">What others have to say:</p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  mode: 'jit',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.scrollbar-thin': {
+          '::-webkit-scrollbar': {
+            width: '4px',
+            height: '4px',
+          },
+          '::-webkit-scrollbar-thumb': {
+            backgroundColor: '#4A5568', // Change to desired thumb color
+            borderRadius: '9999px',
+          },
+          '::-webkit-scrollbar-track': {
+            backgroundColor: '#2D3748', // Change to desired track color
+          },
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 };
 export default config;
