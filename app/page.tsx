04bde665c7f13c1adafa8aec10e761291f890ecd
williamtuominiemi <william.m.tuominiemi@gmail.com>
2024-07-29 21:44:49 +0300

    react-query

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 'use client';
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
 
 import Feed from '@/components/Feed';
 import Controls from '@/components/Controls';
 import Toolbar from '@/components/Toolbar';
 
+const queryClient = new QueryClient();
+
 export default function Home() {
   return (
-    <main className="flex flex-col h-screen w-2/3 bg-gray-800 mx-auto relative rounded-lg">
-      <Feed />
-      <div className="sticky bottom-0 w-full flex flex-col items-end">
-        <Controls />
-        <Toolbar />
-      </div>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className="flex flex-col h-screen w-2/3 bg-gray-800 mx-auto relative rounded-lg">
+        <Feed />
+        <div className="sticky bottom-0 w-full flex flex-col items-end">
+          <Controls />
+          <Toolbar />
+        </div>
+      </main>
+    </QueryClientProvider>
   );
 }
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,34 +1,22 @@
 'use client';
 
+import { useQuery } from '@tanstack/react-query';
+
 import Post from '@/components/Post';
 
-import { useEffect, useState } from 'react';
-
-interface AudioData {
-  id: string;
-  base64: string;
-  createdAt: string;
-}
+// interface AudioData {
+//   id: string;
+//   base64: string;
+//   createdAt: string;
+// }
 
 const Feed = () => {
-  const [audios, setAudios] = useState<AudioData[] | null>(null);
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () => fetch('/api/audio').then((res) => res.json()),
+  });
 
-  useEffect(() => {
-    getAudios();
-  }, []);
-
-  const getAudios = async () => {
-    try {
-      const response = await fetch('/api/audio');
-      if (!response.ok) {
-        throw new Error('Failed to fetch audios');
-      }
-      const data: AudioData[] = await response.json();
-      setAudios(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  console.log(data);
 
   return (
     <div className="flex flex-col justify-center items-center flex-1 overflow-y-auto scrollbar-thin">
@@ -36,13 +24,14 @@ const Feed = () => {
         <h1 className="text-white text-3xl mb-4 pt-5">🗣️ Speakr</h1>
         <p className="text-white text-lg mb-4">What others have to say:</p>
       </div>
-      {audios && (
-        <div className="flex-1 w-1/2 overflow-y-auto">
-          {audios.map((audio) => (
-            <Post key={audio.id} base64={audio.base64} createdAt={audio.createdAt} />
-          ))}
-        </div>
-      )}
+      <div className="flex-1 w-1/2 overflow-y-auto">
+        {isPending && <p className="text-sm">Loading...</p>}
+        {error && <p className="text-sm">An error has occurred: {error.message}</p>}
+        {data && data.length === 0 && <p className="text-sm">No one has posted yet, be the first one.</p>}
+        {data &&
+          data.length > 0 &&
+          data.map((item) => <Post key={item.id} base64={item.base64} createdAt={item.createdAt} />)}
+      </div>
     </div>
   );
 };
