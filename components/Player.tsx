35d28d6503d2e074f94894611e0d1b86803d95f6
williamtuominiemi <william.m.tuominiemi@gmail.com>
2024-07-31 22:08:29 +0300

    Separate button for play and pause

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import React, { useRef } from 'react';
-
+import React, { useRef, useState, useEffect } from 'react';
 import Image from 'next/image';
 
 interface PlayerProps {
@@ -11,21 +10,44 @@ interface PlayerProps {
 const Player: React.FC<PlayerProps> = ({ base64 }) => {
   const audioRef = useRef<HTMLAudioElement>(null);
 
+  const [src, setSrc] = useState('/icons/play.svg');
+
   const handlePlayPause = () => {
     if (audioRef.current) {
       if (audioRef.current.paused) {
         audioRef.current.play();
+        setSrc('/icons/pause.svg');
       } else {
         audioRef.current.pause();
+        setSrc('/icons/play.svg');
       }
     }
   };
 
+  const handleAudioEnded = () => {
+    setSrc('/icons/play.svg');
+  };
+
+  useEffect(() => {
+    const audioElement = audioRef.current;
+
+    if (audioElement) {
+      audioElement.addEventListener('ended', handleAudioEnded);
+
+      return () => {
+        audioElement.removeEventListener('ended', handleAudioEnded);
+      };
+    }
+  }, []);
+
   return (
     <div className="h-full mr-2">
       <audio ref={audioRef} src={base64} />
-      <button className=" bg-emerald-600 hover:bg-emerald-700 p-3 h-full rounded-md " onClick={handlePlayPause}>
-        <Image src="/icons/play-pause.svg" alt="Stop" width={25} height={25} />
+      <button
+        className=" bg-emerald-600 hover:bg-emerald-700 p-3 pl-5 pr-5 h-full rounded-md "
+        onClick={handlePlayPause}
+      >
+        <Image src={src} alt="Stop" width={16} height={16} />
       </button>
     </div>
   );
