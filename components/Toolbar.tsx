cebef7825e308ba48242660db2ce2e990394a937
William Tuominiemi <william.m.tuominiemi@gmail.com>
2024-08-10 12:45:07 +0300

    Fix

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-
 import Image from 'next/image';
+import Link from 'next/link'
 import { FiSettings } from 'react-icons/fi';
 
 import SignInButton from '@/components/SignInButton';
